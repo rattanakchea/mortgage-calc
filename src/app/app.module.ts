cf3a83eaed17bde8662c348e5b8de312b582de96
Rattanak Chea <rattanak.chea@premera.com>
2017-12-25 15:39:29 -0800

    added basic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MortgageFormComponent } from './mortgage-form/mortgage-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MortgageFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mortgage-form/mortgage-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mortgage-form',
+  templateUrl: './mortgage-form.component.html',
+  styleUrls: ['./mortgage-form.component.sass']
+})
+export class MortgageFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
